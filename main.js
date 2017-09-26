82a169514d50cab74e8cb39354086eb48ce35fb8
Chandler <cwm795@gmail.com>
2017-09-26 17:16:38 -0500

    got  boybands and vegtables to run

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
 console.log("hi");
 var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
 var vegtables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+
+var bandLoopCount = bands.length;
+var veggieLoopCount = vegtables.length;
+
+var currentBand = "";
+var currentVeggie = "";
+
+
+var bandElement = document.getElementById("Boybands");
+
+
+
+
+var veggieElement = document.getElementById("vegtables");
+
+
+
+
+for (var i = 0; i < bandLoopCount; i += 1) {
+
+
+
+
+   bandElement.innerHTML += `<p>${bands[i]}</p>`;
+}
+for (var i = 0; i < veggieLoopCount; i += 1) {
+   veggieElement.innerHTML += `<p>${vegtables[i]}</p>`;
+
+
+
+};
