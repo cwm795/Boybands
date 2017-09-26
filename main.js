1586dff1b7b8e9ebab0c7488881cf26313e19722
Chandler <cwm795@gmail.com>
2017-09-25 20:39:48 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,3 @@
+console.log("hi");
+var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+var vegtables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
